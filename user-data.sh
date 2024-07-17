20d03607ad18c6fe245f61bbea8629f39049db3b
Your Name <you@example.com>
2024-07-17 22:45:37 +0200

    code update

--- user-data.sh
+++ user-data.sh
@@ -2,7 +2,7 @@
 
 # Update the system and install necessary packages
 yum update -y
-yum install -y git python3 python3-pip nginx
+yum install -y git python3 python3-pip
 
 # Clone the repository
 REPO_URL="https://github.com/the-mirak/cinedb-aws-demo.git"
@@ -59,57 +59,6 @@ systemctl enable cinedb-app.service
 # Start the Flask application service
 systemctl start cinedb-app.service
 
-# Configure Nginx as a reverse proxy
-cat <<EOF > /etc/nginx/nginx.conf
-user nginx;
-worker_processes auto;
-error_log /var/log/nginx/error.log;
-pid /run/nginx.pid;
-
-events {
-    worker_connections 1024;
-}
-
-http {
-    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
-                      '\$status \$body_bytes_sent "\$http_referer" '
-                      '"\$http_user_agent" "\$http_x_forwarded_for"';
-
-    access_log  /var/log/nginx/access.log  main;
-
-    sendfile            on;
-    tcp_nopush          on;
-    tcp_nodelay         on;
-    keepalive_timeout   65;
-    types_hash_max_size 2048;
-
-    include             /etc/nginx/mime.types;
-    default_type        application/octet-stream;
-
-    upstream flask {
-        server 127.0.0.1:8080;
-    }
-
-    server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        server_name _;
-
-        location / {
-            proxy_pass http://flask;
-            proxy_set_header Host \$host;
-            proxy_set_header X-Real-IP \$remote_addr;
-            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \$scheme;
-        }
-    }
-}
-EOF
-
-# Restart Nginx to apply the new configuration
-systemctl enable nginx
-systemctl restart nginx
 
 # Open port 80 in the firewall (if using firewalld)
 if command -v firewall-cmd > /dev/null; then
